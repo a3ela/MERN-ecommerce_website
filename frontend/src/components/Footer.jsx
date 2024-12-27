ba7e238d243551bafa103697351f52d62a5875a2
a3ela <abelsintayehu626@gmail.com>
2024-12-27 23:03:19 +0300

    feat: new componetents added

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            CopyRight &copy; ProShop {currentYear}
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
