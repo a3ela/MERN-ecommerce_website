99b2d52e1d1550bbe7d9e24e1928dbae57e3aebc
a3ela <abelsintayehu626@gmail.com>
2024-12-29 14:28:57 +0300

    change: products name changed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Row, Col } from "react-bootstrap";
 import products from "../products";
-import Product from "../components/Product";
+import Products from "../components/Products";
 
 const Home = () => {
   return (
@@ -9,7 +9,7 @@ const Home = () => {
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
+            <Products product={product} />
           </Col>
         ))}
       </Row>
