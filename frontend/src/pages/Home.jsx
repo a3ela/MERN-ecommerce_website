80f8047605f828d79676e62e5547dd91835679a4
a3ela <abelsintayehu626@gmail.com>
2024-12-29 20:25:12 +0300

    change: fetching data with axios

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
+import { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
 import Products from "../components/Products";
+import axios from "axios";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,14 +1,23 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import products from "../products";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Rating from "../components/Rating";
 import { FaArrowLeft } from "react-icons/fa";
+import axios from "axios";
 
 const Product = () => {
-  const { id } = useParams();
-  const product = products.find((p) => p._id === id);
-  console.log(product);
+  const [product, setProduct] = useState({});
+  const { id: productId } = useParams();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
+
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
