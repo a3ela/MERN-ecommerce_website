1084506616b885cbbe63ef5fcd576bb257bd136e
a3ela <abelsintayehu626@gmail.com>
2024-12-29 20:21:19 +0300

    change: configured vite to access the backend from localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
+// The Vite configuration file is where you can configure the development server and plugins. In this case, we're using the react plugin and setting up a proxy for the API requests. This is necessary because the frontend and backend are running on different ports. The proxy will forward requests from /api to the backend server running on port 5000. This way, we can avoid CORS issues when making API requests from the frontend.
