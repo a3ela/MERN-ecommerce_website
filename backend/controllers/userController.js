3c7ed2d0151ba0d399a8e267b521a0f84a3a8f9d
a3ela <abelsintayehu626@gmail.com>
2025-01-05 09:21:05 +0300

    feat: logout and register features added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,7 +15,7 @@ const loginUser = middleware.asyncHandler(async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
       sameSite: "Strict",
-      maxAge: "1d",
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
     res.json({
@@ -31,11 +31,41 @@ const loginUser = middleware.asyncHandler(async (req, res) => {
 });
 
 const registerUser = middleware.asyncHandler(async (req, res) => {
-  res.send("register user");
+  const { name, email, password } = req.body;
+
+  const userExist = User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("user already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("Invalid user data");
+  }
 });
 
 const logoutUser = middleware.asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "logged out Successfully" });
 });
 
 const getUserProfile = middleware.asyncHandler(async (req, res) => {
