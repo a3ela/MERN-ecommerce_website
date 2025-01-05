62ec2bbdbf725491b253b395e86d39296e068436
a3ela <abelsintayehu626@gmail.com>
2025-01-05 18:18:30 +0300

    store: for authonication

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./features/apiSlice";
 import cartSliceReducer from "./features/cartSlice";
+import authSliceReducer from "./features/authSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    cart: cartSliceReducer,
+    cart: cartSliceReducer, // Add the cartSlice to the store
+    auth: authSliceReducer, // Add the authSlice to the store
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
