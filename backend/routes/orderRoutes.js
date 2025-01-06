2f02d78802f48e1235abccdadc23e3b25856e161
a3ela <abelsintayehu626@gmail.com>
2025-01-06 13:01:50 +0300

    routes: order routes for the backend

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+  addOrderItems,
+  getMyOrders,
+  getMyOrdersById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+} = require("../controllers/orderController");
+const { protect, admin } = require("../utils/middleware");
+
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
+router.route("/me").get(protect, getMyOrders);
+router.route("/:id").get(protect, admin, getMyOrdersById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
+
+module.exports = router;
