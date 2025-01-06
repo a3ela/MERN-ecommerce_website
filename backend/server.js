f6b12b753f8798c794b13372b474ad370a357e11
a3ela <abelsintayehu626@gmail.com>
2025-01-06 13:02:07 +0300

    add: routes for the order

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,24 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
+const cookieParser = require("cookie-parser");
+
 const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // routes
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+
 app.use(middleware.notFound);
 app.use(middleware.errorHandler);
 
