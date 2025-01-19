41fc30406271145ed079485e38f14f559a357066
a3ela <abelsintayehu626@gmail.com>
2025-01-20 01:42:58 +0300

    feat: to create and update products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,4 +15,40 @@ const getProductsById = asyncHandler(async (req, res) => {
   res.json(product);
 });
 
-module.exports = { getProducts, getProductsById };
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/image/sample.jpg",
+    brand: "sample brand",
+    category: "sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  }
+});
+
+module.exports = { getProducts, getProductsById, createProduct, updateProduct };
