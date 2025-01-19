b6de6d5f40130ac897f3ae10795899f38294c754
a3ela <abelsintayehu626@gmail.com>
2025-01-20 01:47:44 +0300

    feat: to create and update products

--- frontend/src/features/productSLice.js
+++ frontend/src/features/productSLice.js
@@ -15,8 +15,27 @@ export const ProductsApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: PRODUCTS_URL,
+        method: "POST",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data._id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Product"],
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailQuery } =
-  ProductsApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetProductDetailQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+} = ProductsApiSlice;
