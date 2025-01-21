4b0a5420def9e1a0d26f1c216309ce090c7b209b
a3ela <abelsintayehu626@gmail.com>
2025-01-21 12:53:28 +0300

    route: for the product deletion

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,10 +4,15 @@ const {
   getProductsById,
   createProduct,
   updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 const { protect, admin } = require("../middleware/authMiddleware");
 
 routes.route("/").get(getProducts).post(protect, admin, createProduct);
-routes.route("/:id").get(getProductsById).put(protect, admin, updateProduct);
+routes
+  .route("/:id")
+  .get(getProductsById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 module.exports = routes;
