33de5b5f8f5097cb7ffa94a0cccf4a3af9e1da19
a3ela <abelsintayehu626@gmail.com>
2025-01-23 13:00:19 +0300

    rest: the rest of file yet unnessary for the app

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,17 @@ app.get("/api/config/paypal", (req, res) => {
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
